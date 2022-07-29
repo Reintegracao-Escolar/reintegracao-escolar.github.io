f2eec074bf6daf681b971f34cd54b8a0b21a895e
wilberson-roberto <wilberson.roberto.01@gmail.com>
2022-07-28 22:30:24 -0300

    jogo demonstrativo inserido

---
+++ js/jogo-demostrativo.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "De quem é a famosa frase “Penso, logo existo”?",
+    answer: "Descartes",
+    options: [
+      "Platão",
+      "Descartes",
+      "Galileu Galilei",
+      "Socrates"
+    ]
+  },
+    {
+    numb: 2,
+    question: "De onde é a invenção do chuveiro elétrico?",
+    answer: "Brasil",
+    options: [
+      "Alemanha",
+      "China",
+      "Espanha",
+      "Brasil"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quais o menor e o maior país do mundo?",
+    answer: "Vaticano e Rússia",
+    options: [
+      "Vaticano e Rússia",
+      "Vaticano e Brasil",
+      "Mônaco e Canadá",
+      "Mônaco e Rússia"
+    ]
+  },
+    {
+    numb: 4,
+    question: "O que a palavra legend significa em português?",
+    answer: "Lenda",
+    options: [
+      "Legenda",
+      "Legendário",
+      "Lerda",
+      "Lenda"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quem é o autor de “O Príncipe”?",
+    answer: "Maquiavel",
+    options: [
+      "Maquiavel",
+      "Montesquieu",
+      "Thomas Hobbes",
+      "Rousseau"
+    ]
+  },
+];
